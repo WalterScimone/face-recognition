fba01f63b4ed1e871053dd78bd11781e932f7481
WalterScimone <accirasi@protonmail.com>
2021-08-09 17:49:32 -0400

    App.js props and mods update...

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,29 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, Row, Col } from 'react-bootstrap'
+import ImageUploadForm from './components/ImageUploadForm/ImageUploadForm'
+
+//recognize and enroll for endpoint prop...
+const recognize = "recognize"
+const enroll = "enroll"
 
 //react function...
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Facial wreck</h1>
+        <h1>Wal circuit: Recognize Publicly</h1>
       </header>
       <main>
         <Container>
           <Row className="justify-content-md-center">
             <Col md="6">
               <h1>Test person form</h1>
+              <ImageUploadForm endpoint={recognize} />
             </Col>
             <Col md="6">
               <h1>Add person form</h1>
+              <ImageUploadForm endpoint={enroll} />
             </Col>
           </Row>
         </Container>
