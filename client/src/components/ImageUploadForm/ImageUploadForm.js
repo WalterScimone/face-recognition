cdbeb0a79bf8339868a2449763fa85f6393b1685
WalterScimone <accirasi@protonmail.com>
2021-08-14 16:51:09 -0400

    alert messages in upload form...

--- client/src/components/ImageUploadForm/ImageUploadForm.js
+++ client/src/components/ImageUploadForm/ImageUploadForm.js
@@ -1,96 +1,93 @@
-import React, { useState, useCallback } from 'react'
-import { Form, FormControl, Button, } from 'react-bootstrap'
-import classes from './ImageUploadForm.module.css'
-import axios from 'axios'
-
-const ImageUploadForm = (props) => {
-    let [galleryName, setGalleryName] = useState('friend') // set starting value to 'friend'
+import React, { useState, useCallback } from 'react' 
+import { Form, Button, FormControl} from 'react-bootstrap' 
+import classes from './ImageUploadForm.module.css' 
+import axios from 'axios' 
+import Message from '../Message/Message' // NEW 
+const ImageUploadForm = (props) => { 
+    let [alert, setAlert] = useState(null) // NEW 
+    let [file, setFile] = useState(null) 
+    let [fileSrc, setFileSrc] = useState('') 
+    let [galleryName, setGalleryName] = useState('friend')
     let [name, setName] = useState('')
-    let [file, setFile] = useState(null)
-    let [fileSrc, setFileSrc] = useState('')
-
     const fileStaging = useCallback((e) => {
         if (props.type === 'file') {
             setFile(e.target.files[0])
-
+    
             let reader = new FileReader();
-
+    
             try {
                 reader.readAsDataURL(e.target.files[0])
             } catch (e) {
+                setAlert({variant:'danger', text:'Please select valid image.'})  // NEW
                 setFileSrc('')
             }
-
-            reader.addEventListener("load", function () {
+    
+            reader.addEventListener("load", function() {
                 setFileSrc(reader.result)
             }, false)
         } else {
             setFileSrc(e.target.value)
         }
     }, [props.type])
-
     const handleSubmit = (e) => {
-        e.preventDefault() // will stop the page from refreshing on submit
+        e.preventDefault()
         
-        // set alert variable to null
-        // constructs data payload
+        setAlert(null)             // NEW
         let data = {
             gallery_name: galleryName,
             image: fileSrc
         }
-        // adds subject_id if we are enrolling a new person
         if (props.endpoint === 'enroll') {
             data = {
-                ...data, // spread operater - add all the properties in the previously declared data object to this object
+                ...data,
                 subject_id: name
             }
         }
-        console.log(data) // log the data to make sure that it is beng sent the way we want
         axios.post(`/api/upload/${props.endpoint}`, data)
         .then(response => {
-            // set alert variable
+            setAlert(response.data)     // NEW
         })
         .catch(e => {
-            //set alert variable
+            setAlert(e.response.data)   // NEW
         })
     }
-
     return (
         <Form onSubmit={handleSubmit} className="my-4">
+            {alert && <Message message={alert} />}          // NEW
             <Form.Row className="text-left">
                 <Form.Group>
                     <Form.Label>Gallery</Form.Label>
                     <Form.Text className="text-muted">
                         Select upload gallery.
                     </Form.Text>
-                    <Form.Check
+                    <Form.Check 
                         type="radio"
                         label="Friend"
                         checked={galleryName === "friend"}
                         onChange={() => setGalleryName("friend")}
-                    />
-                    <Form.Check
+                        />
+                    <Form.Check 
                         type="radio"
                         label="Foe"
                         checked={galleryName === "foe"}
                         onChange={() => setGalleryName("foe")}
-                    />
+                        />
                 </Form.Group>
             </Form.Row>
             {props.endpoint === 'enroll' ?
-                <Form.Row className="text-left">
-                    <Form.Group>
-                        <Form.Label>Name</Form.Label>
-                        <FormControl
-                            type="text"
-                            placeholder="Subject name"
-                            onChange={(e) => setName(e.target.value)}
-                            value={name}
-                            required
-                        />
-                    </Form.Group>
-                </Form.Row>
-                : null
+            <Form.Row className="text-left">
+                <Form.Group>
+                    <Form.Label>Name</Form.Label>
+                    <FormControl
+                        type="text"
+                        placeholder="Subject name"
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                        required
+                    />
+                </Form.Group>
+            </Form.Row>
+            : null
             }
             <Form.Row>
                 <div className="input-group d-flex justify-content-center mb-3">
@@ -99,13 +96,13 @@ const ImageUploadForm = (props) => {
                     </div>
                     <div className={props.type === 'file' ? "custom-file" : ''}>
                         <label className={props.type === 'file' ? "custom-file-label text-left" : 'text-center'}>{!file ? props.label : file.name}</label>
-                        <input
+                        <input 
                             required
                             type={props.type}
                             className={props.type === 'file' ? "custom-file-input" : "form-control"}
                             placeholder={props.placeholder}
                             onChange={(e) => fileStaging(e)}
-                        />
+                            />
                     </div>
                 </div>
             </Form.Row>
@@ -113,7 +110,5 @@ const ImageUploadForm = (props) => {
             {fileSrc ? <figure><img className={classes.Image} alt="" src={fileSrc} /></figure> : <p style={{ color: "#CCC" }}>No image to preview</p>}
         </Form>
     )
-    
 }
-
 export default ImageUploadForm
