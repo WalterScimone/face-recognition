c3b7130b76a12e13cf2fe2d1b8c2e36c33f498a6
WalterScimone <accirasi@protonmail.com>
2021-08-14 15:24:27 -0400

    routes file for upload...

---
+++ server/src/routes/upload.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+router.route('/enroll').post( async (req, res) => {
+    // enroll route
+})
+router.route('/recognize').post( async (req, res) => {
+    // recognize route
+})
+module.exports = router;
